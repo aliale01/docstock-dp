b4ea4bf804e1513fd240efc8cdb6b376bbf886ab
Alex <alex@Andy-MacBook.local>
2022-09-25 17:03:04 +0200

    User exception

--- src/main/java/com/alex/repo/service/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/alex/repo/service/impl/AuthenticationServiceImpl.java
@@ -1,7 +1,10 @@
 package com.alex.repo.service.impl;
 
+import com.alex.repo.constants.APIServiceError;
+import com.alex.repo.exception.APIExcepiton;
 import com.alex.repo.models.Role;
 import com.alex.repo.models.User;
+import com.alex.repo.repositories.UserRepository;
 import com.alex.repo.service.AuthenticationService;
 import com.alex.repo.service.RoleService;
 import com.alex.repo.service.UserService;
@@ -17,13 +20,17 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private final PasswordEncoder passwordEncoder;
     private final RoleService roleService;
     private final UserService userService;
+    private final UserRepository userRepository;
     @Override
     public User register(String username, String password) {
-        User user = new User();
-        user.setUsername(username);
-        user.setPassword(passwordEncoder.encode(password));
-        Role role = roleService.getRoleByName("USER");
-        user.setRoles(Set.of(role));
-        return userService.create(user);
+        if (userRepository.findUserByUsername(username) != null) {
+            User user = new User();
+            user.setUsername(username);
+            user.setPassword(passwordEncoder.encode(password));
+            Role role = roleService.getRoleByName("USER");
+            user.setRoles(Set.of(role));
+            return userService.create(user);
+        }
+        else throw new APIExcepiton(APIServiceError.USER_EXISTS);
     }
 }
