5afe7bc7ac21b6359524007436bf653153cd448a
Alex <alex@Andy-MacBook.local>
2022-09-26 21:27:27 +0200

    Cors headers and origins

--- src/main/java/com/alex/repo/config/SecurityConfig.java
+++ src/main/java/com/alex/repo/config/SecurityConfig.java
@@ -53,8 +53,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://dp-docstock-mysql.herokuapp.com","http://localhost:8080"));
+        configuration.setAllowedOrigins(Arrays.asList("https://dp-docstock-mysql.herokuapp.com","http://localhost:8080","*"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","HEAD","OPTIONS"));
+        configuration.addAllowedHeader("*");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
